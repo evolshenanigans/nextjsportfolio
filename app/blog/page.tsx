697185de2bf4c121dbd2621642372337fcb35d50
evolshenanigans <gjgutierrez91@gmail.com>
2022-12-07 17:21:20 -0800

    added blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,38 @@
+"use client"
 
+import { useState } from 'react'
 
-function Blog() {
+const Blog = () => {
+  const [posts, setPosts] = useState<{ title: string, content: string }[]>([])
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement> & { target: HTMLFormElement }) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const title = formData.get('title') as string
+    const content = formData.get('content') as string
+    setPosts([...posts, { title, content }])
+  }
+  
+
   return (
-    <div>Blog page</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input type="text" name="title" id="title" />
+        <label htmlFor="content">Content:</label>
+        <textarea name="content" id="content" />
+        <button type="submit">Submit</button>
+      </form>
+      <h2>Blog Posts</h2>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
